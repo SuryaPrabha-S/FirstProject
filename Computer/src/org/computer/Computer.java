41f89f7f1a6a46e25b05488336d93ced810e63e5
User <User@DESKTOP-6O162F6>
2023-01-24 12:36:42 +0530

    Initial commit

---
+++ Computer/src/org/computer/Computer.java
@@ -0,0 +1,13 @@
+package org.computer;
+
+public class Computer extends Desktop {
+private void computerModel() {
+	// TODO Auto-generated method stub
+System.out.println("there are several types of computer model");
+}
+public static void main(String[] args) {
+	Computer co = new Computer();
+	co.computerModel();
+	co.desktopSize();
+}
+}
---
+++ Computer/src/org/computer/Desktop.java
@@ -0,0 +1,12 @@
+package org.computer;
+
+public class Desktop {
+	public void desktopSize() {
+	// TODO Auto-generated method stub
+System.out.println("desktop sizes are vary");
+}
+public static void main(String[] args) {
+	Desktop dt = new Desktop();
+	dt.desktopSize();
+}
+}
